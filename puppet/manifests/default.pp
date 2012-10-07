bc2251aef14fa74dd9fc431abba7d51aa41a59b6
Miro Svrtan <miro@mirosvrtan.me>
2012-10-07 17:43:42 +0200

    Added puppet manifest

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,30 @@
+# Basic Puppet manifest
+
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+class system-update {
+  exec { 'apt-get update':
+    command => 'apt-get update',
+  }
+
+  $sysPackages = [ "build-essential" ]
+  package { $sysPackages:
+    ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+}
+class base-server {
+
+  $devPackages = [ "git", "openssh-server", "dstat", "bmon", "joe", "xz-utils", "rar" ]
+  package { $devPackages:
+    ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+
+}
+
+include system-update
+
+include base-server
+
+
