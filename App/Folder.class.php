600a8b077c571cd6a733b35de31157d2cd6aacf6
Miro Svrtan <miro.svrtan@trikoder.net>
2012-10-15 15:53:40 +0200

    Code2

---
+++ App/Folder.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class File
+{
+    protected  $filePath;
+    
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php
 
-class Folder
-{
-    protected $folderPath;
-
-    public function __construct($folderPath)
-    {
-        $this->folderPath = $folderPath;
-
-    }
-}
-
-class File
-{
-    protected  $filePath;
-    
-    public function __construct($filePath)
-    {
-        $this->filePath = $filePath;
-    }
-}
 
 ?>
