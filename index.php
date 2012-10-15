c1cd60aaedaa73b50576c15db0dca947e50bca53
Miro Svrtan <miro.svrtan@trikoder.net>
2012-10-15 15:51:19 +0200

    Code2

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php 
+<?php
+
+class Folder
+{
+    protected $folderPath;
+
+    public function __construct($folderPath)
+    {
+        $this->folderPath = $folderPath;
+
+    }
+}
+
+class File
+{
+    protected  $filePath;
+    
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+}
 
 ?>
