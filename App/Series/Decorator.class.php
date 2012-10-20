c1852b4449e2bb29d94dc94bfd27202fcd9c3de9
Miro Svrtan <miro.svrtan@trikoder.net>
2012-10-20 19:51:52 +0200

    zz2

--- App/Series/Decorator.class.php
+++ App/Series/Decorator.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Series;
 
+use Exception;
+
 class Decorator
 {
     protected $objFile;
--- App/Series/Worker.class.php
+++ App/Series/Worker.class.php
@@ -22,7 +22,6 @@ class Worker
 
             $this->doWork($objFile);
 
-
         }
 
     }
